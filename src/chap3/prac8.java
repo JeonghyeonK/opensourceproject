461209251a081e8dda4261ef312c824650dcb8c3
JeongHyeonK <6843wjdgus@naver.com>
2021-09-26 22:30:13 +0900

    4주차

--- src/chap3/prac8.java
+++ src/chap3/prac8.java
@@ -1,5 +1,18 @@
 package chap3;
+import java.util.Scanner;
 
 public class prac8 {
-
+/*	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int a;
+		System.out.print("정수 몇 개?");
+		a=scanner.nextInt();
+		int arr = new int[a];
+		for(int i=0; i<a; i++) {
+			arr[i]= (int)(Math.random()*100 +1);
+			
+		}
+		
+	}
+	*/
 }
